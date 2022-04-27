5f72fe9f4642197874b910aaff8686a463efedab
Bruno Cangussu <bruno.cangussu@hotmail.com>
2022-04-27 18:48:01 -0300

    Removed unecessary files from signing server

--- signing/src/index.ts
+++ signing/src/index.ts
@@ -2,9 +2,15 @@ import express from "express";
 import { env } from "./lib/env";
 import enforceAuth from "./middleware/enforceAuth";
 import rootRouter from "./router";
+import cors from "cors";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 app.use(rootRouter);
